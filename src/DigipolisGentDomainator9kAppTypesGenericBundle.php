5adadac3f07f4dc243cfe7c611505baec9781204
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2024-12-05 17:05:37 +0100

    Update to new abstract bundle class

--- src/DigipolisGentDomainator9kAppTypesGenericBundle.php
+++ src/DigipolisGentDomainator9kAppTypesGenericBundle.php
@@ -2,8 +2,8 @@
 
 namespace DigipolisGent\Domainator9k\AppTypes\GenericBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class DigipolisGentDomainator9kAppTypesGenericBundle extends Bundle
+class DigipolisGentDomainator9kAppTypesGenericBundle extends AbstractBundle
 {
 }
